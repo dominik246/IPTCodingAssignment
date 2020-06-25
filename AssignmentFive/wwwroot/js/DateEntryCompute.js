878b2f53c2f502eda7cfd7dcea48364865e1ac2d
Dominik Jelicic <dominik246@gmail.com>
2020-06-25 05:56:33 +0200

    Bug fixes.
    Removed Submit button as it was redundant.

--- AssignmentFive/wwwroot/js/DateEntryCompute.js
+++ AssignmentFive/wwwroot/js/DateEntryCompute.js
@@ -1,10 +1,21 @@
 ﻿async function datePickerSubmit() {
     let form = document.getElementById("datepicker").value;
 
+    if (form === "") {
+        await buildResults(["Please select a date."]);
+        return;
+    }
+
     let birthday = new Date(form);
     let today = Date.now();
 
     let diffTime = today - birthday;
+
+    if (diffTime < 0) {
+        await buildResults(["You can't be born after today.", "Check if you entered the date correctly."]);
+        return;
+    }
+
     let diffMonths = "You are " + (diffTime / (1000 * 60 * 60 * 24 * 30.417)).toFixed(4) + " months old"; // 30.417 je prosjecni broj svih dana u mjesecima u godini
     let diffHours = "or " + (diffTime / (1000 * 60 * 60)).toFixed(4) + " hours";
     let diffDays = "or " + (diffTime / (1000 * 60 * 60 * 24)).toFixed(4) + " days";
@@ -18,6 +29,7 @@
 async function buildResults(arr) {
     let container = document.getElementById("results");
     container.innerHTML = "";
+
     for (let item of arr) {
         let child = document.createElement("p");
         child.innerHTML = item;
